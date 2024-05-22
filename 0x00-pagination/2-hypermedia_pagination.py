1d4ca62a0dbd7ca037753b14ab41e14b3f1abf31
aihassan1 <abdosruor57@gmail.com>
2024-05-22 15:39:49 +0300

    2-hypermedia_pagination.py

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -2,7 +2,7 @@
 """ task 1 Simple pagination
 """
 
-from typing import Tuple, List
+from typing import Tuple, List, Dict
 import csv
 import math
 
@@ -68,3 +68,28 @@ class Server:
 
         page_items = dataset_result[start_idx:end_idx]
         return page_items
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        """returns a dictionary containing the following key-value pairs:
+
+        page_size: the length of the returned dataset page
+        page: the current page number
+        data: the dataset page (equivalent to return from previous task)
+        next_page: number of the next page, None if no next page
+        prev_page: number of the previous page, None if no previous page
+        total_pages: the total number of pages in the dataset as an integer
+        """
+        info = {}
+        page_data = self.get_page(page, page_size)
+        page_len = len(page_data)
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+        start, end = index_range(page, page_size)
+
+        info["page_size"] = page_len
+        info["page"] = page
+        info["data"] = page_data
+        info["next_page"] = page + 1 if page <= total_pages else None
+        info["prev_page"] = page - 1 if start - 1 > 0 else None
+        info["total_pages"] = total_pages
+
+        return info
