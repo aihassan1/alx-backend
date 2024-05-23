f3ef95dcc9859ff9ae0af74ab8c967d9ba0c0e55
aihassan1 <abdosruor57@gmail.com>
2024-05-23 06:15:09 +0300

    3

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,37 +1,44 @@
 #!/usr/bin/env python3
-"""Task 3: LRU Caching
-"""
-
-
-from collections import OrderedDict
+"""task 3. LRU Caching"""
 from base_caching import BaseCaching
 
 
 class LRUCache(BaseCaching):
-    """A class `LRUCache` that inherits from
-    `BaseCaching` and is a caching system
-    """
+    """LRUCache class"""
 
     def __init__(self):
-        """initialize the cache"""
         super().__init__()
-        self.cache_data = OrderedDict()
-
-    def put(self, key, item):
-        """Adds an item in the cache."""
-        if key is None or item is None:
-            return
-        if key not in self.cache_data:
-            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
-                lru_key, _ = self.cache_data.popitem(True)
-                print("DISCARD:", lru_key)
-            self.cache_data[key] = item
-            self.cache_data.move_to_end(key, last=False)
-        else:
-            self.cache_data[key] = item
+        self.cache_order = []
 
     def get(self, key):
-        """Retrieves an item by key."""
-        if key is not None and key in self.cache_data:
-            self.cache_data.move_to_end(key, last=False)
-        return self.cache_data.get(key, None)
+        """Add an item in the cache"""
+
+        if key is None:
+            return None
+        if self.cache_data.get(key, None) is not None:
+            self.cache_order.remove(key)
+            self.cache_order.insert(0, key)
+            return self.cache_data.get(key, None)
+        else:
+            return None
+
+    def put(self, key, item):
+        """put an item"""
+        if key is None or item is None:
+            pass
+        else:
+            if key in self.cache_order:
+                self.cache_order.remove(key)
+
+            if len(self.cache_order) >= BaseCaching.MAX_ITEMS:
+                print(
+                    f"DISCARD: {self.cache_order[-1]} , value :{ self.cache_data[self.cache_order[-1]]} "
+                )
+                del self.cache_data[self.cache_order[-1]]
+                self.cache_order.pop(-1)
+                self.cache_order.insert(0, key)
+                self.cache_data[key] = item
+
+            else:
+                self.cache_order.insert(0, key)
+                self.cache_data[key] = item
