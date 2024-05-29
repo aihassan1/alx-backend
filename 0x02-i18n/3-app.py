89117a7c0a08219e6fcbf4874437e9d5966f7b85
aihassan1 <abdosruor57@gmail.com>
2024-05-29 07:08:14 +0300

    .

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python3
-"""task 3 This Python script sets up a basic Flask web application with
-(i18n) support using Flask-Babel."""
+"""
+This Python script sets up a basic Flask web application with
+internationalization (i18n) support using Flask-Babel.
+"""
 from flask import Flask, render_template, request
 from flask_babel import Babel, _
 
 
 class Config:
-    """Config class for locale and timezone"""
+    """
+    Configuration class for Flask application.
+
+    Attributes:
+        LANGUAGES (list): The languages supported by the application.
+        BABEL_DEFAULT_LOCALE (str): The default language locale.
+        BABEL_DEFAULT_TIMEZONE (str): The default timezone.
+    """
 
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
@@ -22,13 +31,23 @@ babel = Babel(app)
 
 @babel.localeselector
 def get_locale() -> str:
-    """get locale for a page"""
+    """
+    Determine the best match for user's accepted languages.
+
+    Returns:
+        str: The best language match from accepted languages.
+    """
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
 @app.route("/")
 def home() -> str:
-    """home page"""
+    """
+    Render the home page.
+
+    Returns:
+        str: HTML of the home page.
+    """
     return render_template("3-index.html")
 
 
