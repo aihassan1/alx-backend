554e07bbfe99db5d3646c4b799af4b6a864be705
aihassan1 <abdosruor57@gmail.com>
2024-05-29 07:22:02 +0300

    .

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
-"""Task 4: Force locale with URL parameter
 """
-
+This Python script sets up a basic Flask web application with
+internationalization (i18n) support using Flask-Babel.
+"""
 from flask import Flask, render_template, request
-from flask_babel import Babel
+from flask_babel import Babel, _
 
 
 class Config:
-    """Config class"""
+    """
+    Configuration class for Flask application.
 
-    DEBUG = True
+    Attributes:
+        LANGUAGES (list): The languages supported by the application.
+        BABEL_DEFAULT_LOCALE (str): The default language locale.
+        BABEL_DEFAULT_TIMEZONE (str): The default timezone.
+    """
+
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -18,37 +25,35 @@ class Config:
 app = Flask(__name__)
 app.config.from_object(Config)
 app.url_map.strict_slashes = False
+
 babel = Babel(app)
 
 
 @babel.localeselector
 def get_locale() -> str:
-    """Retrieves the locale for a web page.
+    """
+    Determine the best match for user's accepted languages.
 
     Returns:
-        str: best match
+        str: The best language match from accepted languages.
     """
     locale = request.args.get("locale")
-    if locale in app.config["LANGUAGES"]:
+    if locale and locale in app.config["LANGUAGES"]:
         return locale
+
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
 @app.route("/")
-def index() -> str:
-    """default route
+def home() -> str:
+    """
+    Render the home page.
 
     Returns:
-        html: homepage
+        str: HTML of the home page.
     """
     return render_template("4-index.html")
 
 
-# uncomment this line and comment the @babel.localeselector
-# you get this error:
-# AttributeError: 'Babel' object has no attribute 'localeselector'
-# babel.init_app(app, locale_selector=get_locale)
-
-
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
