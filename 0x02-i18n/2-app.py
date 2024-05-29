a490d6dfb7b6d861d53f7dd9ba6913e2849f174a
aihassan1 <abdosruor57@gmail.com>
2024-05-29 06:02:43 +0300

    2-app.py

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-"""Task 2: Get locale from request
-"""
-
+"""task 2"""
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
@@ -9,7 +7,6 @@ from flask_babel import Babel
 class Config:
     """Config class"""
 
-    DEBUG = True
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -18,28 +15,21 @@ class Config:
 app = Flask(__name__)
 app.config.from_object(Config)
 app.url_map.strict_slashes = False
+
 babel = Babel(app)
 
 
 @babel.localeselector
-def get_locale() -> str:
-    """Retrieves the locale for a web page.
-
-    Returns:
-        str: best match
-    """
+def get_locale():
+    """get locale"""
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
 @app.route("/")
-def index() -> str:
-    """default route
-
-    Returns:
-        html: homepage
-    """
-    return render_template("2-index.html")
+def home():
+    """home page"""
+    return render_template("1-index.html")
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
